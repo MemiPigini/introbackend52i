103be25ea300ab1ecf1d08c5fce47b0558abddca
Maria Emilia Pigini <piginimaemilia@gmail.com>
2023-09-25 20:09:32 +0200

    Backend

--- database/config.js
+++ database/config.js
@@ -6,7 +6,8 @@ const dbConnection = async () => {
         console.log("Base de datos conectada");
     }catch (error){
         console.log(error);
-        throw new Error ("Error en base de datos") //para que aparezca el mensaje de error en pantalla
+        throw new Error ("Error en base de datos");
+        //para que aparezca el mensaje de error en pantalla
     }
 }
 module.exports = {
